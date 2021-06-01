a022577571040b1e56748a881486ea69a7a68a21
Martina Milli <millimartina8@gmail.com>
2021-06-01 09:44:11 +0200

    Auth guard premjesten samo na parent komponentu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthComponent } from './components/auth/auth.component';
 import { LoggedInGuard } from './services/logged-in.guard';
 import { HomeComponent } from './components/home/home.component';
 import { PasswordResetComponent } from './components/password-reset/password-reset.component';
+import { AuthGuard } from './services/auth.guard';
 
 
 const routes: Routes = [
@@ -11,7 +12,7 @@ const routes: Routes = [
   {path: 'auth', component: AuthComponent, canActivate: [LoggedInGuard]},
   {path: 'home', component: HomeComponent, canActivate: [LoggedInGuard]},
   {path: 'passwordReset', component: PasswordResetComponent, canActivate: [LoggedInGuard]},
-  {path: 'contact', loadChildren: () => import('./modules/contacts/contact.module').then(m => m.ContactsModule)},
+  {path: 'contact', loadChildren: () => import('./modules/contacts/contact.module').then(m => m.ContactsModule), canActivate: [AuthGuard]},
 ];
 
 @NgModule({
--- src/app/modules/contacts/contact.routing.module.ts
+++ src/app/modules/contacts/contact.routing.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailsComponent } from './components/details/details.component';
 import { MyContactsComponent } from './components/my-contacts/my-contacts.component';
-import { AuthGuard } from '../../services/auth.guard';
 import { UnsavedChangesGuardService } from './services/unsavedChanges.guard';
 import { FormComponent } from './components/form/form.component';
 import { ContactComponent } from './contact.component';
 
 
 const routes: Routes = [
-    {path: '', component: ContactComponent, canActivate: [AuthGuard]},
-    {path: 'my-contacts', component: MyContactsComponent, canActivate: [AuthGuard]},
+    {path: '', component: ContactComponent},
+    {path: 'my-contacts', component: MyContactsComponent},
     {path: 'new', component: FormComponent, canDeactivate: [UnsavedChangesGuardService]},
-    {path: ':id/details', component: DetailsComponent, canActivate: [AuthGuard]},
+    {path: ':id/details', component: DetailsComponent},
     {path: ':id/edit', component: FormComponent, canDeactivate: [UnsavedChangesGuardService]},
 ];
 
