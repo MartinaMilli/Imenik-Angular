14776e077bc6207895b572dc748c25fefa73790b
Martina Milli <millimartina8@gmail.com>
2021-06-01 16:28:50 +0200

    Dodano filtriranje po stupcu tablice

--- src/app/modules/contacts/components/my-contacts/my-contacts.component.ts
+++ src/app/modules/contacts/components/my-contacts/my-contacts.component.ts
@@ -1,9 +1,9 @@
-import { AfterViewChecked, AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { Contact } from '../../models/contact.model';
@@ -21,11 +21,13 @@ export class MyContactsComponent implements OnInit, AfterViewInit, OnDestroy {
   dataSource = new MatTableDataSource<Contact>();
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
-  filterStringValue = '';
+  @ViewChild('filterInput')  filterInput: ElementRef;
   displayedColumns: string[] = ['firstName', 'lastName', 'email', 'details', 'edit', 'delete'];
+  filterColumns: string[] = ['Ime', 'Prezime', 'E-mail adresa'];
+  filterProperty = '';
+  selected = '';
 
 
-  // keep track of changes in the contactService
   contactSub: Subscription;
   fetchingSub: Subscription;
   isFetching: boolean;
@@ -33,12 +35,10 @@ export class MyContactsComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor(
     private contactService: ContactService,
     private router: Router,
-    private route: ActivatedRoute,
     public dialog: MatDialog,
     private authService: AuthService) { }
 
   ngOnInit(): void {
-
     // dohvacanje podataka kada se korisnik ulogira
     if (this.authService.loggedIn) {
       this.contactService.fetchContacts();
@@ -48,7 +48,7 @@ export class MyContactsComponent implements OnInit, AfterViewInit, OnDestroy {
     this.dataSource.data = this.contactService.contactList;
     this.contactSub = this.contactService.contactsChanged.subscribe(contacts => {
       this.dataSource.data = contacts;
-      this.resetFilter();
+      this.resetTable();
     });
     this.paginator._intl.itemsPerPageLabel = 'Prikazano po stranici';
 
@@ -64,13 +64,34 @@ export class MyContactsComponent implements OnInit, AfterViewInit, OnDestroy {
     this.dataSource.sort = this.sort;
   }
 
-  onFilter(): void {
-    this.dataSource.filter = this.filterStringValue;
+  onFilter(filterValue: string): void {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  private resetFilter(): void {
+  setupFilter(): any {
+    switch (this.selected) {
+      case 'Ime':
+        this.filterProperty = 'firstName';
+        break;
+      case 'Prezime':
+        this.filterProperty = 'lastName';
+        break;
+      case 'E-mail adresa':
+        this.filterProperty = 'email';
+        break;
+      default: this.filterProperty = 'firstName';
+    }
+    this.dataSource.filterPredicate = (d: Contact, filter: string) => {
+      const textToSearch = d[this.filterProperty] && d[this.filterProperty].toLowerCase() || '';
+      return textToSearch.indexOf(filter) !== -1;
+    };
+  }
+
+  private resetTable(): void {
+    const table = document.getElementById('tbl');
+    table.scrollLeft = 0;
     this.dataSource.filter = '';
-    this.filterStringValue = '';
+    this.filterInput.nativeElement.value = '';
   }
 
   onDetails(id: string): void {
